b3ca557298e380a54f7c25ae47a4050a1d5aa770
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-04-30 14:46:31 +0530

    [sanjana] Add : Random number array find second largest and second smallest with sorting

--- ArrayPracticeProblem.js
+++ ArrayPracticeProblem.js
@@ -1,46 +1,18 @@
-for(let i=0;i<10;i++)
-{
-    a[i]=Math.floor(Math.random()*999)+100;
+
+let a = [];
+let temp = 0;
+for (let i = 0; i < 10; i++) {
+    a[i] = Math.floor(Math.random() * 999) + 100;
 }
-FindSecondLargest();
-FindSecondSmallest();
-function FindSecondLargest()
-{
-    let secondmax=0;
-    let max=0;
-    for(let i=0;i<10;i++)
-    {
-       if(a[i]>max)
-       {
-        secondmax=max;
-        max=a[i];
-    }
-    {
-        if(a[i]<max && a[i]>secondmax)
-        {
-            secondmax=a[i];
+for (let i = 0; i < 10; i++) {
+   
+    for (let j = i; j < 10; j++) {
+        if (a[i] < a[j]) {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
         }
     }
-    }
-    console.log("Second largest number is----->"+secondmax);
 }
-function FindSecondSmallest()
-{
-    let secondmin=999;
-    let min=999;
-    for(let i=0;i<10;i++)
-    {
-       if(a[i]<min)
-       {
-        secondmin=min;
-        min=a[i];
-    }
-    {
-        if(a[i]!=min && a[i]<secondmin)
-        {
-            secondmin=a[i];
-        }
-    }
-    }
-    console.log("Second smallest number is----->"+secondmin);
-}
+console.log("Second largest number is----->"+a[1]);
+console.log("Second smallest number is----->"+a[a.length-2]);
