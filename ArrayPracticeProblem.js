b8bebd6bcf1b84603f06205d56db0134c6867d93
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-04-30 14:52:26 +0530

    [sanjana] Add : Showing addtion of three numbers is zero

--- ArrayPracticeProblem.js
+++ ArrayPracticeProblem.js
@@ -1,38 +1,14 @@
-let n=15;
-arr=[];
-let k=0;
-for (let i = 2; i <=n; i++) {
-  if(n % i == 0)
-  {
-    CheckPrime(i);
-  }
-}
-
-function CheckPrime(a)
+let arr = [1, 0, 2, -1, 5, -7, -2];
+for (let i = 0; i < arr.length; i++) 
 {
-    let flag =0;
-    
-    for(let i = 2; i < a/2; i++ )
-    {
-        if(a%i ==0)
+    for (let j = i; j < arr.length; j++)
+     {
+        for (let k = j; k < arr.length; k++) 
         {
-            flag=1;
-            break;
+            if(arr[i]+arr[j]+arr[k] == 0)
+            {
+                console.log(arr[i] +" + "+arr[j] +" + "+arr[k] +" = 0");
+            }
         }
-        else
-        {
-            flag=0;
-           
-        }
-       
     }
-    if(flag == 0)
-    {
-        arr[k]=a;
-        k++;
-    }
-}
-for(let i=0;i<arr.length;i++)
-{
-    console.log(arr[i]);
 }
